3d380c75648b94fc18eb7e2a81e17e43919b4be4
zanjs <root@zanjs.com>
2017-07-31 17:02:19 +0800

    Those who turn back never reach the summit. :bug:

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 export default class Storage {
+  /**
+   * 获取指定 key 的值
+   * @static
+   * @param {any} key 
+   * @returns 
+   * @memberof Storage
+   */
+  static get(key) {
+    let value = undefined;
+    let store = localStorage.getItem(key);
+    if (store) {
+      store = JSON.parse(store);
+      value = store;
+    }
+    return value;
+  }
+  /**
+   * 设置用户 Token 信息
+   * @static
+   * @param {any} token 
+   * @param {any} user 
+   * @param {any} account 
+   * @memberof Storage
+   */
   static setUser(token, user, account) {
-    const v = {token, user, account};
+    const v = { token, user, account };
     localStorage.setItem('token', JSON.stringify(v));
   }
   static getValue(key) {
@@ -16,13 +40,13 @@ export default class Storage {
     let store = localStorage.getItem('token');
     if (store) {
       store = JSON.parse(store);
-      const v = {account: store['account']};
+      const v = { account: store['account'] };
       localStorage.setItem('token', JSON.stringify(v));
     }
   }
   static updateUser(user) {
     let store = localStorage.getItem('token');
-    if(store) {
+    if (store) {
       store = JSON.parse(store);
       store.user = user;
       localStorage.setItem('token', JSON.stringify(store));
